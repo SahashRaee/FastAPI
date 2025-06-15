3c369b78136ef3575462064e73a196e8ea84539d
sahash07 <raikapilkhutru9863329002@gmail.com>
2025-06-15 10:05:41 +0545

    Data Validation

--- Pydantic/1. Pydantic_Basic.py
+++ Pydantic/1. Pydantic_Basic.py
@@ -1,15 +1,21 @@
-from pydantic import BaseModel                  
-
-class Student(BaseModel):
+#@ DEPENDENCIES:                     
+from pydantic import BaseModel                                                  # Importing BaseModel class from pydantic for data validation            
+from typing import List, Dict, Optional , TypedDict ,                                        
+#@ MODEL DEFINITON:
+class Student(BaseModel):                                                       # Defining the student model inheriting from BaseModel
     name: str 
     age: int
+    married: Optional[str] = None
 
-def insert_student_data(student: Student):
+def insert_student_data(student: Student):                                      # Function to insert and display student data
     print(student.age)
     print(student.name)
     print("Data added")
 
-student_info = {'name': 'Sahash', 'age': 19}
-student1 = Student(**student_info)
+#@ DATA PREPARATION:
+student_info = {'name': 'Sahash', 'age': 19}                                    # Dictionary holding student information
 
-insert_student_data(student1)
+#@ MODEL INSTANTIATION
+student1 = Student(**student_info)                                              # Creating an instance of Student class using unpacked dictionary
+
+insert_student_data(student1)                                                   # Function calling
